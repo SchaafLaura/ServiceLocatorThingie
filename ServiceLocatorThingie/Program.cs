79e1414657b42cf86ce340d33bee47dca340afe1
SchaafLaura <schaaf.laura92@gmail.com>
2025-04-03 15:58:57 +0200

    Examples and Exceptions

--- ServiceLocatorThingie/Program.cs
+++ ServiceLocatorThingie/Program.cs
@@ -1,21 +1,37 @@
 ﻿using ServiceLocatorThingie;
 
-
-
+//////////////////////////// Adding new Services Example //////////////////////////// 
 Services.Open();
-Services.TryRegister<AudioService>(new(0));
+Services.TryRegister<AudioService>(new(soundId: 0));
+Services.TryRegister<AudioService>(new(soundId: 1));
 Services.Close();
 
 var hasAudio = Services.TryLocate<AudioService>(out var audioService);
 if(hasAudio)
-    audioService!.PlaySong();
+    audioService!.PlaySong(); // plays song 0
 
-
-
+//////////////////////////// Overriding Services Example ////////////////////////////
 Services.Open();
-Services.ForceAdd<AudioService>(new(1));
+Services.ForceAdd<AudioService>(new(soundId: 1));
 Services.Close();
 
 hasAudio = Services.TryLocate<AudioService>(out audioService);
 if(hasAudio)
-    audioService!.PlaySong();
+    audioService!.PlaySong();  // plays song 1
+
+//////////////////////////// Exceptions Example 1 ////////////////////////////
+Services.Open();
+try
+{
+    hasAudio = Services.TryLocate<AudioService>(out audioService); // throws ServiceLocatorNotClosedException
+}
+catch (Exception _) { }
+Services.Close();
+
+//////////////////////////// Exceptions Example 2 ////////////////////////////
+try
+{
+    Services.TryRegister<AudioService>(new(soundId: 0));    // throws ServiceLocatorNotOpenException
+    Services.ForceAdd<AudioService>(new(soundId: 1));       // throws ServiceLocatorNotOpenException
+} 
+catch (Exception _) { }
--- ServiceLocatorThingie/Services.cs
+++ ServiceLocatorThingie/Services.cs
@@ -3,20 +3,22 @@ using System.Collections.Frozen;
 namespace ServiceLocatorThingie;
 internal static class Services
 {
-    private static Dictionary<Type, object>? _registry = [];
+    private static Dictionary<Type, object>? _registry;
     private static FrozenDictionary<Type, object>? registry;
 
-    public static bool IsFinal = true;
+    public static bool IsFinal { get; private set; } = true;
     
     public static bool TryRegister<T>(T service) where T : class
     {
-        return !IsFinal && _registry!.TryAdd(typeof(T), service);
+        if (IsFinal)
+            throw new ServiceLocatorNotOpenException();
+        return _registry!.TryAdd(typeof(T), service);
     }
 
     public static void ForceAdd<T>(T service) where T : class
     {
         if (IsFinal)
-            return;
+            throw new ServiceLocatorNotOpenException();
 
         _registry![(typeof(T))] = service;
     }
@@ -24,10 +26,7 @@ internal static class Services
     public static bool TryLocate<T>(out T? t)
     {
         if (!IsFinal)
-        {
-            t = default;
-            return false;
-        }
+            throw new ServiceLocatorNotClosedException();
 
         t = default;
         var ret = registry!.TryGetValue(typeof(T), out var x);
@@ -40,10 +39,18 @@ internal static class Services
     public static void Open()
     {
         IsFinal = false;
+        
+        _registry = registry is null ? [] : registry.ToDictionary();
+        registry = null;
     }
     public static void Close()
     {
-        registry = _registry!.ToFrozenDictionary();
         IsFinal = true;
+        
+        registry = _registry!.ToFrozenDictionary();
+        _registry = null;
     }
 }
+
+internal class ServiceLocatorNotClosedException : Exception;
+internal class ServiceLocatorNotOpenException : Exception;
